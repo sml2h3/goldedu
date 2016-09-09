3558a532e6dfe2e5a38704add5083d3ee0cdcfcc
Sml2h3 <sml2h3@gmail.com>
2016-09-09 19:57:57 +0800

    通过composer加入xunsearch模块

--- config/app.php
+++ config/app.php
@@ -147,6 +147,7 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
+        Fook\xunsearch\XsServiceProvide::class,
 
         /*
          * Application Service Providers...
@@ -201,6 +202,7 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+        'XS' => Fook\xunsearch\XS::class,
 
     ],
 
